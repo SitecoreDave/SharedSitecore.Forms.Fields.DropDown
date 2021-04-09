e1b9b3837be135d43d783212a6f35496ef69b9a3
David Walker <dave@radicaldave.com>
2021-04-09 09:24:19 -0500

    init

---
+++ src/DropDownViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+using System;
+
+namespace SharedSitecore.Forms.Fields.DropDown
+{
+	[Serializable]
+	public class DropDownViewModel : ListViewModel
+	{
+		public bool ShowEmptyItem { get; set; }
+		public bool ShowValues { get; set; }		
+
+		protected override void InitItemProperties(Item item)
+		{
+			Assert.ArgumentNotNull(item, nameof(item));
+			base.InitItemProperties(item);
+
+			ShowEmptyItem = MainUtil.GetBool(item.Fields["Show Empty Item"]?.Value, defaultValue: false);
+			ShowValues = MainUtil.GetBool(item.Fields["Show Values"]?.Value, defaultValue: false);          
+        }
+
+        protected override void UpdateItemFields(Item item)
+		{
+			Assert.ArgumentNotNull(item, nameof(item));
+			base.UpdateItemFields(item);
+
+			item.Fields["Show Empty Item"]?.SetValue(ShowEmptyItem ? "1" : string.Empty, force: false);
+			item.Fields["Show Values"]?.SetValue(ShowValues ? "1" : string.Empty, force: false);
+		}
+	}
+}
---
+++ src/Pipelines/ExecuteSubmit.cs
@@ -0,0 +1,72 @@
+﻿using Sitecore;
+using Sitecore.Data;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Mvc;
+using Sitecore.ExperienceForms.Mvc.Pipelines.ExecuteSubmit;
+using Sitecore.Mvc.Pipelines;
+using System.Linq;
+
+namespace SharedSitecore.Forms.Fields.DropDown.Pipelines
+{
+    public class ExecuteSubmit : MvcPipelineProcessor<ExecuteSubmitActionsEventArgs>
+    {
+        private readonly IFormRenderingContext _formRenderingContext;
+
+        /// <summary>
+        /// ID of the Field on the Form
+        /// </summary>
+        public string FieldId { get; set; }
+
+        public ExecuteSubmit(IFormRenderingContext renderingContext)
+        {
+            _formRenderingContext = renderingContext;
+        }
+
+        public override void Process(ExecuteSubmitActionsEventArgs args)
+        {
+            Assert.ArgumentNotNull(FieldId, nameof(FieldId));
+            if (!args.FormSubmitContext.Fields.Any(f => f.Name == nameof(DropDown))) return;
+
+            var fields = args.FormSubmitContext.Fields.Where(f => f.Name == nameof(DropDown));
+            if (fields != null) 
+            {
+                foreach (var field in fields)
+                {
+                    ReplaceTokensIfApplicable((DropDownViewModel)field);
+                }
+            }
+        }
+
+        protected virtual void ReplaceTokensIfApplicable(DropDownViewModel viewModel)
+        {
+            Assert.ArgumentNotNull(viewModel, nameof(viewModel));
+            Assert.ArgumentNotNull(viewModel.Value, nameof(viewModel.Value));
+            if (viewModel.ShowValues || viewModel.IsDynamic) return;
+
+            var value = string.Join(",", viewModel.Value);
+            if (string.IsNullOrEmpty(value)) return;
+
+            var values = value.Split(',');
+            if (values == null) return;
+
+            foreach (var v in values)
+            {
+                if (!ID.TryParse(v, out ID valueId)) continue;
+
+                if (!ID.IsNullOrEmpty(valueId))
+                {
+                    var it = Context.Database.GetItem(valueId);
+                    if (it != null)
+                    {
+                        var itemValue = !string.IsNullOrEmpty(viewModel.ValueFieldName) && it.Fields[viewModel.ValueFieldName] != null ? it.Fields[viewModel.ValueFieldName].ToString() : it.DisplayName;
+
+                        if (!string.IsNullOrEmpty(itemValue) && itemValue != (viewModel.Value == null ? string.Empty : string.Join(",", viewModel.Value)))
+                        {
+                            viewModel.Value = itemValue.Split(',').ToList();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
